4da551472adbed8659eff6419dd39ad888eab62c
RH <incredincomp@gmail.com>
2018-11-26 10:48:45 -0800

    Update greprow2.sh

--- greprow2.sh
+++ greprow2.sh
@@ -62,7 +62,7 @@ FILEPATH=`$DIALOG --stdout --title "Please choose a file" --fselect $HOME/ 14 48
 case $? in
 	0)
 		clear
-		echo "\"$FILEPATH\" chosen";;
+		echo "\$FILEPATH" chosen";;
 	1)
 		clear
 		echo "Cancel pressed.";;
@@ -71,70 +71,12 @@ case $? in
 		echo "Box closed.";;
 esac
 }
-#get_Path2 () {
-#this is a function set up to call a dialog box for user to select a file to parse 
-#prompt="Please select a file:"
-#options=( $(find -maxdepth 1 -print0 | xargs -0) )
 
-#PS3="$prompt "
-#select input in "${options[@]}" "Quit" ; do
-#        if (( REPLY > 0 && REPLY <= ${#options[@]} )) ; then
-#	    echo "You picked $input which is file $REPLY"
-#	    break
-#		
-#		
-#	elif (( REPLY == 1 + ${options[@]} )) ; then
-#	    exit
-#		
-#	else
-#	    echo "Invalid option. Try again." ;
-#	fi
-#done
-#}
 
-#read -p "Please type your full file path, starting with a backslash if it is absolute. It's more than likely equal to $PWD/file.txt: " inputPath
-
-get_Path () {
-clear
-printf " If you would like to define your own path, please press y.  Pressing n will set your path as $PWD/log.txt. "
-echo "  "
-printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-while true; do
-    echo "  "
-    echo -n "y or n: " 
-    read answer
-#    echo "  "
-#    echo "______________________________________________________________"
-    printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-    case $answer in
-# in theory, the "return 1 and 0's" should allow this to error check in a sense. to be tested, 
-# https://unix.stackexchange.com/questions/268764/how-to-repeat-prompt-to-user-in-a-shell-script
-                [yY] )
-#                       clear
-                       echo "__________________________________________________"
-                       read -p "Please type your full file path, starting with a backslash if it is absolute. It's more than likely equal to $PWD/file.txt: " inputPath
-                       return 0
-                       ;;
-
-                [nN] )
-#                       clear
-                       echo "Okay, we're going to just use $PWD/log.txt for you."
-                       FILEPATH="$PWD/log.txt"
-	    	       return 0
-		       ;;
-
-                   * ) 
-#                       clear
-	               echo "Invalid input"
-                       return 1
-		       ;;
-    esac
-done
-}
 
 what_Find () { 
 echo "	"
-echo -n "What linformation would you like to find? "
+echo -n "What information would you like to look for? "
 read lookFor
 echo "	"
 echo "Looking for $lookFor... Please wait... "
